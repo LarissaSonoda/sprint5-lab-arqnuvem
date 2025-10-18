3efa160b0caead30f486c7bf01fc30e8896281ce
Larissa Miranda Sonoda <82535458+LarissaSonoda@users.noreply.github.com>
2025-10-18 00:12:28 -0300

    Update S3 bucket name variable for deployment

--- sprint5-lab/infra/variables.tf
+++ sprint5-lab/infra/variables.tf
@@ -2,7 +2,7 @@ variable "aws_region" {
  default = "us-east-1"
 }
 variable "bucket_name" {
- default = "lab-sprint5-arqnuvem-lari-sonoda"
+ default = "lab-sprint5-arqnuvem-lari-sonoda-2"
 }
 variable "key_name" {
   default = "lab7"  
